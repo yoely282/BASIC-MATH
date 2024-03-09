57b218eba94bee7f5cf642c15790be3b31e6c7f1
Yoel Yoel <147493769+yoely282@users.noreply.github.com>
2024-03-08 20:47:42 -0600

    Add files via upload

---
+++ BASIC MATH.py
@@ -0,0 +1,33 @@
+print ("Adding Basic Math Numbers") 
+num1 = 10
+num2 = 24
+result = num1 + num2
+print ("10 + 24")
+print(result)  
+
+
+
+print("Subtracting Basic Math Numbers")
+num1 = 45
+num2 = 35
+result = num1 - num2 
+print ("45 - 35 ")
+print(result)
+
+
+print("Multiplying Basic Numbers")
+num1 = 60
+num2 = 200
+result = num1 * num2 
+print ("60 * 200")
+print(result)
+
+
+print("Dividing Basic Numbers")
+num1 = 500
+num2 = 0
+if num2 != 0:
+    result = num1 / num2
+    print(result)
+else:
+    print('Sorry, cannot divide 0 as a denominator')
